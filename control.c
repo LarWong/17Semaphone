66d046fff97417d9b85eb6c6c061f26d2df27b88
LarWong <lwong7@stuy.edu>
2018-12-13 16:47:32 -0500

    control done

--- control.c
+++ control.c
@@ -7,6 +7,7 @@
 #include <sys/stat.h>
 #include <sys/shm.h>
 #include <fcntl.h>
+#include <unistd.h>
 
 union semun {
   int val; // for SETVAL
@@ -16,9 +17,6 @@ union semun {
 };
 
 int main(int argc, char* argv[]){
-
-  key_t semkey,shmkey;
-  int shmid,semid;
     
   if (argc != 2){
     printf("Incorrect number of arguments\n");
@@ -30,7 +28,7 @@ int main(int argc, char* argv[]){
     key_t shmkey = ftok("./", 65);
     int semid = semget(semkey, 1, IPC_CREAT | IPC_EXCL | 0644);
     int shmid = shmget(shmkey, 1024, IPC_CREAT | IPC_EXCL | 0644);
-    int fp = open("story.txt",O_TRUNC|O_CREAT,0666);
+    int fd = open("story.txt",O_TRUNC|O_CREAT,0666);
 
     struct sembuf x;
     x.sem_num = 0;
@@ -42,10 +40,37 @@ int main(int argc, char* argv[]){
   }
 
   if (!strcmp(argv[1],"-r")){
+    key_t semkey = ftok("./",65);
+    key_t shmkey = ftok("./",65);
+    int semid = semget(semkey, 1,0644);
+    int shmid = shmget(shmkey, 1024, 0644);
+    char * data = shmat(shmkey, 0, 0);
     
+    int fd = open("story.txt",O_RDONLY);
+
+    char text[10000];
+    read(fd, text, 10000);
+    printf("File Contains:  %s\n", text);
+
+    shmdt(data);
+    shmctl(shmid,IPC_RMID,NULL);
+    semctl(semid,0,IPC_RMID);
+
+    close(fd);
+    remove("story.txt");
 
 
   }
+
+  if (!strcmp(argv[1],"-v")){
+
+    int fd = open("story.txt",O_RDONLY);
+    char text[10000];
+    read(fd, text, 10000);
+    printf("File Contains:  %s\n", text);
+
+  }
+  
   
 
   return 0;
